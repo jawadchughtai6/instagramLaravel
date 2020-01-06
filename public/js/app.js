8d39935537c7c831546cc4b7cbcb72eba494b9dc
Jawad <jawadchughtai6@gmail>
2020-01-06 19:56:24 +0300

    frontend-headerandphotos1

--- public/js/app.js
+++ public/js/app.js
@@ -49613,8 +49613,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\7.4.1\htdocs\instagram\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\7.4.1\htdocs\instagram\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! d:\7.4.1\htdocs\instagram\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! d:\7.4.1\htdocs\instagram\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,35 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-4">
+            <img style="height: 150px;" src="https://instagram.fruh4-5.fna.fbcdn.net/v/t51.2885-19/s320x320/22220615_1967856660020739_3845756632151621632_n.jpg?_nc_ht=instagram.fruh4-5.fna.fbcdn.net&_nc_ohc=J-EgFaUXNe4AX9iPhzz&oh=356ed803c5ac5922a487a596bcfd41bb&oe=5EAAF9FD" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+           <div>
+               <h1>jawadcc7</h1>
+           </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>32 </strong>posts</div>
+                <div class="pr-5"><strong>140 </strong>followers</div>
+                <div class="pr-5"><strong>198 </strong>following</div>
             </div>
+            <div class="pt-3 font-weight-bold">Jawad Chughtai</div>
+            <div>Be together. Not the same. <br>
+                Jeddah. NUST.
+            </div>
+            <div><a href="https://www.linkedin.com/in/jawad-chughtai-904716168/">LinkedIn Profile</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fjed4-3.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.120.960.960a/s640x640/46462813_2092490107501409_3557638437205532747_n.jpg?_nc_ht=instagram.fjed4-3.fna.fbcdn.net&_nc_cat=105&_nc_ohc=odY-4KqvjBoAX_Mdbh1&oh=9f3bd6395637ca37f04943897391e86e&oe=5E97ADB9" class="w-100" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fjed4-3.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c180.0.720.720a/s640x640/19623443_248264189002444_5592797217602666496_n.jpg?_nc_ht=instagram.fjed4-3.fna.fbcdn.net&_nc_cat=104&_nc_ohc=rEml080XDv0AX-IdLBL&oh=ec649fcc97d53263bb6b85925ca51168&oe=5EA7D9A3" class="w-100" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fjed4-5.fna.fbcdn.net/v/t51.2885-15/e35/c234.0.611.611a/19932258_2158680024358335_5554106618896973824_n.jpg?_nc_ht=instagram.fjed4-5.fna.fbcdn.net&_nc_cat=103&_nc_ohc=S-4ucfXis8oAX_xIbKe&oh=cb66b2dc776e71752f03837ad7a8cc4b&oe=5E9246AB" class="w-100" alt="">
         </div>
     </div>
 </div>
